4e54c2c47e0949dd668621ff96b3b27e2272c9fd
Jonathan Mutakura <jmutakura@gmail.com>
2020-01-18 18:17:44 -0600

    Did the post loops

--- blog/main_blog/views.py
+++ blog/main_blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Jonathan',
+        'title': 'Blog post 1',
+        'content': 'First content posted',
+        'date': 'January 18 2020'
+    },
+{
+        'author': 'James Loo',
+        'title': 'Blog post 2',
+        'content': 'Second content posted',
+        'date': 'January 28 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'main_blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'main_blog/about.html')
